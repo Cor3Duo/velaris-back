85e7fe7f2b872e904bf2b21753f3c10aeae0d79d
Richard Caetano <richard@foodpass.me>
2026-05-19 10:37:17 -0300

    fix: prisma config

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,11 @@
 import { defineConfig } from "prisma/config";
 import { config } from "dotenv";
 
-config();
+config(); 
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
   datasource: {
-    url: process.env.DIRECT_URL!, 
+    url: process.env.DIRECT_URL || "", 
   },
 });
